7a6ebc1329bcee8023bdc89bb414fced709185c2
jinghua-ibmcloud <jinghua@ibmcloud-master>
2023-08-06 05:16:04 +0000

    prometheus adminapi bug fix, added snapshot script

--- k8s_cluster_setup/cluster_setup_src/master/7_k8s_tools_install.sh
+++ k8s_cluster_setup/cluster_setup_src/master/7_k8s_tools_install.sh
@@ -14,9 +14,12 @@ kubectl apply -f $k8s_cluster_setup_pdir/k8s_cluster_setup/k8s_tools/kube-promet
 # install kepler
 kubectl apply -f $k8s_cluster_setup_pdir/k8s_cluster_setup/k8s_tools/kepler/_output/generated-manifest/deployment.yaml
 
-# enable memory snapshot, recommended for taking prometheus TSDB snapshots later.
+echo "waiting for 10s for prometheus to start..."
 sleep 10
+# enable memory snapshot, recommended for taking prometheus TSDB snapshots later.
 kubectl -n monitoring patch prometheus k8s --type merge --patch '{"spec":{"enableFeatures":["memory-snapshot-on-shutdown"]}}'
+# enable admin api for taking snapshots later
+kubectl -n monitoring patch prometheus k8s --type merge --patch '{"spec":{"enableAdminAPI":true}}'
 
 # create new screen sessions for looping-forever kubectl prometheus port forwarding
 # Note: a screen can be killed by command: screen -XS screen_id quit
@@ -28,10 +31,10 @@ if ! screen -list | grep -q pmt_port_forward; then
 else 
     echo "prometheus port forwarding screen detected, assuming it is working normally..."
 fi
-if ! screen -list | grep -q pmt_port_keep_alive; then
-    echo "starting port forwarding keeping alive screen..."
-	# screen -S pmt_port_keep_alive -d -m -t ./runtime_scripts/port_forward_keep_alive.sh
-	screen -S pmt_port_forward -d -m bash -c "while true; do nc -vz 127.0.0.1 9090; sleep 10; done;"
-else 
-    echo "port forwarding keeping alive screen detected, assuming it is working normally..."
-fi
+# if ! screen -list | grep -q pmt_port_keep_alive; then
+#     echo "starting port forwarding keeping alive screen..."
+# 	# screen -S pmt_port_keep_alive -d -m -t ./runtime_scripts/port_forward_keep_alive.sh
+# 	screen -S pmt_port_keep_alive -d -m bash -c "while true; do nc -vz 127.0.0.1 9090; sleep 10; done;"
+# else 
+#     echo "port forwarding keeping alive screen detected, assuming it is working normally..."
+# fi
---
+++ k8s_cluster_setup/cluster_setup_src/master/EXPORT_PROMETHEUS_SNAPSHOT.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash 
+# this script needs to be executed on the master node in root account.
+
+source 0_settings.sh
+
+pmt_pod_name="prometheus-k8s-1"
+snapshot_target_dir="/root/prometheus_snapshots-2nd/"
+
+echo "getting information from pod: $pmt_pod_name"
+pmt_pod_ip=`kubectl get pods -n monitoring -o wide | grep $pmt_pod_name | awk '{ print $6 }'`
+echo "pod internal IP is: $pmt_pod_ip"
+
+echo "deleting previous existing snapshots on pod: $pmt_pod_name"
+kubectl exec -it -n monitoring $pmt_pod_name -- rm -r /prometheus/snapshots
+
+echo "sending request for generating prometheus snapshot on pod: $pmt_pod_name"
+pmt_pod_snapshot_gen_result=`curl -XPOST http://$pmt_pod_ip:9090/api/v1/admin/tsdb/snapshot`
+echo "prometheus snapshot generation result: $pmt_pod_snapshot_gen_result"
+
+if [[ $pmt_pod_snapshot_gen_result == *"success"* ]]; then
+    echo "snapshot generated successfully, trying to clean up destination folder: $snapshot_target_dir"
+    rm -r $snapshot_target_dir
+    echo "copying results from $pmt_pod_name to $snapshot_target_dir"
+    kubectl cp -n monitoring $pmt_pod_name:/prometheus/snapshots/ $snapshot_target_dir
+else
+    echo "snapshot generation failed"
+fi
